7d1cb52c4ccf0f390c968d797fef5745d97e8940
KedarKandel <kandelkedar07@gmail.com>
2023-04-30 14:42:02 +0300

    create route to get information on specific journey/ distance in km and duration in minutes

--- backend/routes/journey.js
+++ backend/routes/journey.js
@@ -29,4 +29,31 @@ router.get('/getAll', async (req, res) => {
   }
 });
 
+
+// Get bike journey details by ID
+router.get('/:id', async (req, res) => {
+    try {
+      const journeyId = parseInt(req.params.id);
+      const { rows } = await db.query(
+        `
+        SELECT 
+          departure_station_name, 
+          return_station_name,
+          covered_distance_m/1000 AS distance_km,
+          duration_s/60 AS duration_minutes
+        FROM journeys
+        WHERE id = $1
+        `,
+        [journeyId]
+      );
+      if (rows.length === 0) {
+        return res.status(404).json({ message: 'Journey not found' });
+      }
+      res.json(rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    }
+  });
+
 export default router
