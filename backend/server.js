b807c86e0a800bf65090a25ca666a0289ed4a090
KedarKandel <kandelkedar07@gmail.com>
2024-02-11 22:59:39 +0200

    edit express static path

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ async function loadData(){
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>/*/
 
-app.use(express.static(path.join(__dirname, "../../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // routes
 import journeysRoute from "./routes/journey.js"
@@ -46,7 +46,7 @@ app.use("/api/v1/stations", stationsRoute)
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 app.listen(PORT, ()=>{
